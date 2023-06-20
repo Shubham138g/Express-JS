3b78789243e00f92102d9e43b25788bd4fddf295
shubham <tonygupta275@gmail.com>
2023-06-20 14:05:16 +0530

    send html element via routing

--- 003-html-data.js
+++ 003-html-data.js
@@ -1,6 +1,8 @@
 const express =require ("express");
 const app = express();
 
+
+// How To Send HTML & JSON Data as a Response using Express JS
 app.get("/",(req,res)=> {
      res.write("<h1>hello from th learning<h1>");
      res.write("<h1>hello from the express js learning</h1>");
