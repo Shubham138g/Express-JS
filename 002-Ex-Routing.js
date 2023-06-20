1c00bd40a46ac3aea99010c6867ca62aea12845d
shubham <tonygupta275@gmail.com>
2023-06-20 13:45:50 +0530

    intial commit

---
+++ 002-Ex-Routing.js
@@ -0,0 +1,21 @@
+const express=require ("express");
+const app=express();
+
+app.get("/",(req,res)=>{
+   res.send("welcome to home page")
+})
+
+app.get("/about",(req,res)=>{
+    res.send("hello from about page")
+})
+app.get("/help",(req,res)=>{
+    res.send("hello from help page")
+})
+app.get("/contact",(req,res)=>{
+    res.send("hello from contact page")
+})
+
+const PORT=3000;
+app.listen(PORT,()=>{
+    console.log(`server is runnig on port number ${PORT}`)
+})
