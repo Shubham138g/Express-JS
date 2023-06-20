a5fdec46398b74dd4e7d14b69ac3758c2b597d3c
shubham <tonygupta275@gmail.com>
2023-06-20 14:24:24 +0530

    modify

--- 003-html-data.js
+++ 003-html-data.js
@@ -8,8 +8,8 @@ app.get("/", (req, res) => {
     res.write("<h1>hello from the express js learning</h1>");
     res.send();
 })
-app.get("/json", (req, res) => {
-    res.send([
+app.get("/json", (req, res) => {          //first method to send json data (res.json())
+    res.json([
         {                                 //array of objects
             id: 1,
             name: "shubham",
@@ -21,9 +21,29 @@ app.get("/json", (req, res) => {
         {
             id: 3,
             name: "shubham",
+        },
+        {
+            id: 4,
+            name: "shubham",
         }
     ]);
 });
+// app.get("/json", (req, res) => {          //second method to send json data (res.send())
+//     res.send([
+//         {                                 //array of objects
+//             id: 1,
+//             name: "shubham",
+//         },
+//         {
+//             id: 2,
+//             name: "shubham",
+//         },
+//         {
+//             id: 3,
+//             name: "shubham",
+//         }
+//     ]);
+// });
 const port = 3000;
 app.listen(port, () => {
     console.log(`server is runnig on port number ${port}`)
