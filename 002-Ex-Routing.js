de19ff003f8fcb1c27a69760e7a75e581e3b8492
shubham <tonygupta275@gmail.com>
2023-06-20 13:46:45 +0530

    intial commit

--- 002-Ex-Routing.js
+++ 002-Ex-Routing.js
@@ -1,8 +1,8 @@
 const express=require ("express");
 const app=express();
 
-app.get("/",(req,res)=>{
-   res.send("welcome to home page")
+app.get("/",(req,res)=>{  //app.get(route,callback_function)
+   res.send("welcome to home page")  
 })
 
 app.get("/about",(req,res)=>{
