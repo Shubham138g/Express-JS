970f29eace82ae8d3e38b0b3bb142f66d591bd51
shubham <tonygupta275@gmail.com>
2023-06-22 17:44:59 +0530

    partial tutorial

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 const path=require('path');
 const express =require ("express");
 const app = express();
+const hbs= require('hbs');
+const port=8000;
 
 const staticPath= path.join(__dirname,"../public");
-const templatePath= path.join(__dirname,"../templates");
+const templatePath= path.join(__dirname,"../templates/views");
+const partialsPath= path.join(__dirname,"../templates/partials")
 
 
 //to set the view engine
 app.set('view engine','hbs');
 app.set("views",templatePath);
+hbs.registerPartials(partialsPath);
+
 
 //template engine route
 app.get("/",(req,res)=>{
@@ -25,6 +30,6 @@ app.get("/",(req,res)=> {
      res.send("hello from the express js learning")
 })
 
-app.listen(8000, ()=>{
-    console.log("server is runnig")
+app.listen(port, ()=>{
+    console.log(`server is runnig on ${port}`)
 })
